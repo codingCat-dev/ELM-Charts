fe070f9ba53da6c03f84ac7015f202bbcc083eff
Antonio Sulejman <tocycz@gmail.com>
2022-04-15 09:31:43 +0200

    feat:commit, add 4 items to charts

---
+++ src/Main.elm
@@ -0,0 +1,29 @@
+module Main exposing (..)
+
+import Chart as C
+import Chart.Attributes as CA
+import Svg as S
+
+
+main =
+    C.chart
+        [ CA.width 900
+        , CA.height 300
+        , CA.margin { top = 50, right = 50, bottom = 50, left = 50 }
+        ]
+        [ C.bars []
+            [ C.bar .calories [ CA.color "#336699" ]
+            , C.bar .kj [ CA.color "#339966" ]
+            ]
+            data
+        , C.xLabels []
+        , C.yLabels [ CA.withGrid ]
+        ]
+
+
+data =
+    [ { x = 1, calories = 52.0, kj = 218.0 }
+    , { x = 2, calories = 160.0, kj = 672.0 }
+    , { x = 3, calories = 47.0, kj = 197.0 }
+    , { x = 4, calories = 88.0, kj = 478.0 }
+    ]
